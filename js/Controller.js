80189e3b0b13243265886831a7372f988eff6ce3
Sangdang2203 <sangdang2203@gmail.com>
2023-01-16 17:18:00 +0700

    Second Update

--- js/Controller.js
+++ js/Controller.js
@@ -1,1 +1,19 @@
 var app = angular.module("myApp", ["ngRoute"]);
+    app.config(function($routeProvider){
+        $routeProvider
+        .when ("/",{
+            templateUrl : "Home.html"
+        })
+        .when ("/product",{
+            templateUrl : "Product.html",
+            controller : "myCtrl"
+        })
+        .when ("/about",{
+            templateUrl : "AboutUs.html",
+            controller : "myCtrl"
+        })
+        .when ("/contact",{
+            templateUrl : "ContactUs.html",
+            controller : "myCtrl"
+        });
+    });
