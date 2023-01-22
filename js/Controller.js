c2d17a57adb63f588a431b311fe3eb2830a1fc92
Sangdang2203 <sangdang2203@gmail.com>
2023-01-23 02:34:45 +0700

    4th Update

--- js/Controller.js
+++ js/Controller.js
@@ -19,5 +19,55 @@ var app = angular.module("myApp", ["ngRoute"]);
         .when ("/signin",{
             templateUrl : "SignIn.html",
             controller : "myCtrl"
+        })
+        .when ("/CellingLights",{
+            templateUrl : "CellingLights.html",
+            controller : "myCtrl"
+        })
+        .when ("/WallLights",{
+            templateUrl : "WallLights.html",
+            controller : "myCtrl"
+        })
+        .when ("/Lamps",{
+            templateUrl : "Lamps.html",
+            controller : "myCtrl"
+        })
+        .when ("/OutdoorLights",{
+            templateUrl : "OutdoorLights.html",
+            controller : "myCtrl"
+        })
+        .when ("/Fans",{
+            templateUrl : "Fans.html",
+            controller : "myCtrl"
+        })
+        .when ("/HomeAccent",{
+            templateUrl : "HomeAccent.html",
+            controller : "myCtrl"
+        })
+        .when ("/SpotLights",{
+            templateUrl : "SpotLights.html",
+            controller : "myCtrl"
+        })
+        .when ("/DecorationLights",{
+            templateUrl : "DecorationLights.html",
+            controller : "myCtrl"
+        })
+        .when ("/SmartLights",{
+            templateUrl : "SmartLights.html",
+            controller : "myCtrl"
         });
+        
+    });
+    app.controller('myCtrl', function($scope, $http) {
+        function getData (){
+            $http.get('decorationLights.json').then(function(rspt) {
+                if(sessionStorage.getItem("decorationLights") == null){
+                    sessionStorage.setItem("decorationLights", JSON.stringify(rspt.data));
+                    $scope.decorationLightsList = JSON.parse(sessionStorage.getItem("decorationLights"));
+                }else{
+                    $scope.decorationLightsList = JSON.parse(sessionStorage.getItem("decorationLights"));    
+                }
+            });
+        }
+        getData();
     });
